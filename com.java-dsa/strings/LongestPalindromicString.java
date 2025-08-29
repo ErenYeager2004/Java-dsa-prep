7cf3bef4326f9e8875248879fa64f05a16ff2e96
Debjit Saha <debjitsaha606@gmail.com>
2025-08-29 21:08:11 +0530

    String problem error fixed

--- com.java-dsa/strings/LongestPalindromicString.java
+++ com.java-dsa/strings/LongestPalindromicString.java
@@ -1,4 +1,63 @@
 package strings;
 
+import java.util.Scanner;
+
+//Problem -> Returns the longest substring of s that is a palindrome
 public class LongestPalindromicString {
+    static String longestPalindromicStringBrute(String s) {
+        //T.C -> O(n3)
+        int maxlen = 0;
+        String result = "";
+        for(int i=0; i<s.length(); i++) {
+            for(int j=i; j<s.length(); j++) {
+                String sub  = s.substring(i,j+1);
+                if(isPalindrome(sub) && sub.length() > maxlen){
+                    result = sub;
+                    maxlen = sub.length();
+                }
+            }
+        }
+        return result;
+    }
+
+    private static boolean isPalindrome(String sub) {
+        int l = 0, r = sub.length()-1;
+        while(l<r) {
+            if(sub.charAt(l++) != sub.charAt(r--)) return false;
+        }
+        return true;
+    }
+
+
+    //Optimized-Expand around Center--O(n2)
+    static String longestPalindromicString(String s) {
+        if(s == null || s.isEmpty()) return "";
+        int start = 0, end = 0;
+        for(int i=0; i<s.length(); i++) {
+            int len1 = expand(s,i,i);
+            int len2 = expand(s,i,i+1);
+            int len = Math.max(len1,len2);
+            if(len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len/2;
+            }
+        }
+        return s.substring(start,end+1);
+    }
+
+    private static int expand(String s, int left, int right) {
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(longestPalindromicStringBrute(str));
+        System.out.println(longestPalindromicString(str));
+
+    }
 }
--- com.java-dsa/strings/ValidAnagram.java
+++ com.java-dsa/strings/ValidAnagram.java
@@ -1,4 +1,39 @@
 package strings;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ValidAnagram {
+    static boolean isValidAnagramBrute(String s, String t) {
+        // Brute force O(n log n)
+        if(s.length() != t.length()) return false;
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
+    }
+
+    static boolean isValidAnagram(String s, String t) {
+        //using array hashing O(n)
+        if(s.length() != t.length()) return false;
+        int[] count = new int[26];
+        for(int i=0; i<s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for(int i : count) {
+            if(i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        System.out.println(isValidAnagramBrute(s,t));
+        System.out.println(isValidAnagram(s,t));
+    }
 }
