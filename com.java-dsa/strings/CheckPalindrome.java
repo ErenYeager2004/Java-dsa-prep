5fd4e758be63d061384129504e04169c9369c79b
Debjit Saha <debjitsaha606@gmail.com>
2025-08-31 21:56:15 +0530

    Check if a string is palindrome or not

--- com.java-dsa/strings/CheckPalindrome.java
+++ com.java-dsa/strings/CheckPalindrome.java
@@ -1,4 +1,11 @@
 package strings;
 
 public class CheckPalindrome {
+    static boolean isPalindrome(String str) {
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+    public static void main(String[] args) {
+        String str = "Madam";
+        System.out.println(isPalindrome(str));
+    }
 }
