786770b15ecbd8cf0933b5f9335c14686da7902a
Debjit Saha <debjitsaha606@gmail.com>
2025-09-01 21:28:09 +0530

    check if a number is prime or not

--- com.java-dsa/basicConceptsProblems/PrimeNumber.java
+++ com.java-dsa/basicConceptsProblems/PrimeNumber.java
@@ -1,4 +1,16 @@
 package basicConceptsProblems;
 
 public class PrimeNumber {
+    static boolean isPrime(int number) {
+        for(int i=2; i<=Math.sqrt(number); i++) {
+            if(number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int number = 11;
+        System.out.println(isPrime(number));
+    }
 }
