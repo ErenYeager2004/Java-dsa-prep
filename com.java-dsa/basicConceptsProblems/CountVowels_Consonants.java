0254a27bcc34b5a1e1032a25672f54a5b5ace54a
Debjit Saha <debjitsaha606@gmail.com>
2025-09-01 21:27:09 +0530

    check how many vowels and consonants present in the given string

--- com.java-dsa/basicConceptsProblems/CountVowels_Consonants.java
+++ com.java-dsa/basicConceptsProblems/CountVowels_Consonants.java
@@ -1,4 +1,22 @@
 package basicConceptsProblems;
 
 public class CountVowels_Consonants {
+    static void countVowels_Consonents(String str) {
+        int vowel = 0, consonant = 0;
+        for(char ch : str.toCharArray()) {
+            if(Character.isDigit(ch)) {
+                break;
+            } else if ("aeiouAEIOU".indexOf(ch) != -1) {
+                vowel ++;
+            }
+            else{
+                consonant++;
+            }
+        }
+        System.out.println("Vowels :"+vowel + " Consonant :"+consonant);
+    }
+    public static void main(String[] args) {
+        String str = "Automation";
+        countVowels_Consonents(str);
+    }
 }
