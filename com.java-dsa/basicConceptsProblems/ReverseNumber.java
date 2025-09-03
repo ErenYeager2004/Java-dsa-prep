c035161fd4e559c07c59d0b2089a3d5bbe9e490a
Debjit Saha <debjitsaha606@gmail.com>
2025-09-03 11:51:33 +0530

    reverse a give number

--- com.java-dsa/basicConceptsProblems/ReverseNumber.java
+++ com.java-dsa/basicConceptsProblems/ReverseNumber.java
@@ -1,4 +1,16 @@
 package basicConceptsProblems;
 
 public class ReverseNumber {
+    static int reverse(int n) {
+        int rev = 0;
+        while(n != 0){
+            rev = rev * 10 + n % 10;
+            n /= 10;
+        }
+        return rev;
+    }
+    public static void main(String[] args) {
+        int number  =  1234;
+        System.out.println(reverse(number));
+    }
 }
