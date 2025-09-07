2cc38df6936000b3d8269c5cfc83f965ad21ace8
Debjit Saha <debjitsaha606@gmail.com>
2025-09-07 23:09:16 +0530

    cognizent practice sheet problem 3 solved

--- com.java-dsa/cognizent_practice_sheet/Problem3.java
+++ com.java-dsa/cognizent_practice_sheet/Problem3.java
@@ -1,4 +1,22 @@
 package cognizent_practice_sheet;
 
+import java.util.Scanner;
+
 public class Problem3 {
+    static void magicConverter(int[] arr){
+        for (int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]+"-"+(char)arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+        System.out.print("Enter the digits:");
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<arr.length; i++){
+            int x = sc.nextInt();
+            arr[i] = x;
+        }
+
+        magicConverter(arr);
+    }
 }
