7a07cf1b13a2937a5b380cc5849c862b3128fef6
Debjit Saha <debjitsaha606@gmail.com>
2025-09-07 23:08:59 +0530

    cognizent practice sheet problem 2 solved

--- com.java-dsa/cognizent_practice_sheet/Problem2.java
+++ com.java-dsa/cognizent_practice_sheet/Problem2.java
@@ -1,4 +1,25 @@
 package cognizent_practice_sheet;
 
+import java.util.Scanner;
+
 public class Problem2 {
+    static void generateBill (int pizza, int puffs, int drinks) {
+        System.out.println("Bill Details");
+        System.out.println("No of pizzas:"+pizza);
+        System.out.println("No of puffs:"+puffs);
+        System.out.println("No of cooldrinks:"+drinks);
+        System.out.println("Total price="+((pizza*100)+(puffs*20)+(drinks*10)));
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no of pizzas bought:");
+        int no_of_pizza = sc.nextInt();
+        System.out.print("Enter the no of puffs bought:");
+        int no_of_puffs = sc.nextInt();
+        System.out.print("Enter the no of cool drinks bought:");
+        int no_of_coldDrink = sc.nextInt();
+
+        generateBill(no_of_pizza,no_of_puffs,no_of_coldDrink);
+    }
 }
