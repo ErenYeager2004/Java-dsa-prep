a06e923c7b4199b9fd9a03820ecc6abafce62169
Debjit Saha <debjitsaha606@gmail.com>
2025-09-08 21:05:18 +0530

    solving problem 8

--- com.java-dsa/cognizent_practice_sheet/Problem8.java
+++ com.java-dsa/cognizent_practice_sheet/Problem8.java
@@ -1,4 +1,28 @@
 package cognizent_practice_sheet;
 
+import java.util.Scanner;
+
 public class Problem8 {
+    static boolean isPalindrome(int n) {
+        int org = n;
+        int rev = 0;
+        while(n != 0) {
+            rev = rev * 10 + n%10;
+            n /= 10;
+        }
+        return org == rev;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if(isPalindrome(number)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+        }
+    }
 }
+
+
+
+
