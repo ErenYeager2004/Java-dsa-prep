b722aff14237b96fdbe7f66240e3e10f1ca24c33
Debjit Saha <146350889+ErenYeager2004@users.noreply.github.com>
2025-09-16 05:18:26 +0000

    quick sort

---
+++ com.java-dsa/sorting/QuickSort.java
@@ -0,0 +1,50 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int partition = findPartition(arr, low, high);
+            quickSort(arr, low, partition);
+            quickSort(arr, partition + 1, high);
+        }
+    }
+
+    static int findPartition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+
+            while (arr[i] <= pivot && i <= high - 1) {
+                i++;
+            }
+
+            while (arr[j] > pivot && j >= low + 1) {
+                j--;
+            }
+
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // int temp = arr[i];
+        // arr[i] = arr[j];
+        // arr[j] = temp;
+        return j;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 9, 2, 3, 1, 6, 7, 2, 0, 1 };
+        int low = 0;
+        int high = arr.length - 1;
+        quickSort(arr, low, high);
+        System.out.println(Arrays.toString(arr));
+    }
+}
