30eddbe0ec55ab0f42da2d095464ac657754cfeb
ErenYeager2004 <debjitsaha606@gmail.com>
2025-09-19 18:28:18 +0530

    add a second method to find a string is a palindrome

--- com.java-dsa/strings/CheckPalindrome.java
+++ com.java-dsa/strings/CheckPalindrome.java
@@ -1,11 +1,19 @@
 package strings;
 
 public class CheckPalindrome {
+    static boolean isPalindrome2(String str) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = str.length() - 1; i >= 0; i--) {
+            sb.append(str.charAt(i));
+        }
+        return str.contentEquals(sb);
+    }
     static boolean isPalindrome(String str) {
-        return str.equals(new StringBuilder(str).reverse().toString());
+        return str.contentEquals(new StringBuilder(str).reverse());
     }
     public static void main(String[] args) {
         String str = "Madam";
         System.out.println(isPalindrome(str));
+        System.out.print(isPalindrome2(str));
     }
 }
